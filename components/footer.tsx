4f1c798d288242c3addf8b74ca75fa9825130299
Vinay Kumar Basava <44405756+vinay956@users.noreply.github.com>
2022-07-21 12:05:38 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer: NextPage = () => {
   return (
     <footer className="footer">
-      <h1 className="logo">T E D P A Y</h1>
+      <h1 className="logo">UPI Pay</h1>
       <p className="slogan">
         There is a technical issue with {" "} Please Use Other UPI Apps
         <img
@@ -18,6 +18,7 @@ const Footer: NextPage = () => {
         <Link href={"/privacy-policy"} passHref>
           <u>Privacy Policy</u>
         </Link>
+      </p>
     </footer>
   );
 };
