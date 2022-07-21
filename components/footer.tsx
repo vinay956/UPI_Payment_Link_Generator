4370675a2e94b2279ba826dde665d75a8f0f9317
Vinay Kumar Basava <44405756+vinay956@users.noreply.github.com>
2022-07-21 12:20:32 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,10 +4,7 @@ import Link from "next/link";
 const Footer: NextPage = () => {
   return (
     <footer className="footer">
-      <br>
-      <br>
-      <br>
-      <p className="slogan">
+      <p className="slogan"style="page-break-before: always">
         There is a technical issue with Phonepe Please Use Other UPI Apps
       </p>
       <h1 className="logo">UPI Pay</h1>
