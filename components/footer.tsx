0690bef10650bb7641d159aa1a40e2acaef975b7
Vinay Kumar Basava <44405756+vinay956@users.noreply.github.com>
2022-07-21 12:39:39 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,6 +7,30 @@ const Footer: NextPage = () => {
       <h3 className="slogan">
         <br/>There is a technical issue with Phonepe <br/>Please Use Other UPI Apps
       </h3>
+       <img
+           src="https://www.svgrepo.com/show/303357/google-pay-primary-logo-logo.svg"
+           className="upilogo"
+           width="60"
+           alt="Upi Icon"
+          />
+      <img
+           src="https://www.svgrepo.com/show/303357/google-pay-primary-logo-logo.svg"
+           className="upilogo"
+           width="60"
+           alt="Upi Icon"
+          />
+      <img
+           src="https://www.svgrepo.com/show/303357/google-pay-primary-logo-logo.svg"
+           className="upilogo"
+           width="60"
+           alt="Upi Icon"
+          />
+      <img
+           src="https://www.svgrepo.com/show/303357/google-pay-primary-logo-logo.svg"
+           className="upilogo"
+           width="60"
+           alt="Upi Icon"
+          />
       <h1 className="logo">UPI Pay</h1>
       <p className="tpf">
         <Link href={"/privacy-policy"} passHref>
